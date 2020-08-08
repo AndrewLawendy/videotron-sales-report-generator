10102e93bdf03c7f7f9779a6137487883f63d996
andrew-hani <andrew.lawendy@outlook.com>
2020-08-08 22:47:42 +0200

    🎉 Initiate base configuration

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'semantic-ui-css/semantic.min.css'
+
+
+const App = () => {
+    return (<div>Working...</div>)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
