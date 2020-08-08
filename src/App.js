ed4dbd19b2b53c198561214ed5b21d334dc3ed4c
andrew-hani <andrew.lawendy@outlook.com>
2020-08-08 22:54:03 +0200

    🎨 Format

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'semantic-ui-css/semantic.min.css'
-
+import "semantic-ui-css/semantic.min.css";
 
 const App = () => {
-    return (<div>Working...</div>)
-}
+  return <div>Working...</div>;
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
