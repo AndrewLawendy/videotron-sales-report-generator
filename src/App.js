605c3541a9135ad122671e97f0365bba7bc3d8c9
andrew-hani <andrew.lawendy@outlook.com>
2020-09-06 22:25:42 +0200

    Revert "🐛 Fixing download link"
    This reverts commit dc188674a6bfbccc63b1f713e32a52315b359a8d.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import "semantic-ui-css/semantic.min.css";
 
 import "./general.scss";
 
+import zipApp from "../assets/Ventes-PAV.zip";
+
 import { Container, Header, Button, Icon } from "semantic-ui-react";
 
 const App = () => {
   return (
     <Container>
       <Header>Appuyez sur Télécharger pour obtenir l&apos;application</Header>
-      <a
-        download
-        title="Télécharger"
-        href="https://github.com/AndrewLawendy/videotron-sales-report-generator/raw/ELEC-DOWNLOAD/assets/Ventes-PAV.zip"
-      >
+      <a download title="Télécharger" href={zipApp}>
         <Button primary size="huge" icon labelPosition="left">
           <Icon name="cloud download"></Icon>
           Télécharger
