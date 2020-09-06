73c06b703d59dbcc2528ceb6fa611fbd519fe97d
andrew-hani <andrew.lawendy@outlook.com>
2020-09-06 22:08:10 +0200

    🎨 Prepare a view to download the app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
 
+import "./general.scss";
+
+import { Container, Header, Button, Icon } from "semantic-ui-react";
+
 const App = () => {
-  return <div>Working...</div>;
+  return (
+    <Container>
+      <Header>Appuyez sur Télécharger pour obtenir l&apos;application</Header>
+      <a download title="Télécharger" href="#">
+        <Button primary size="huge" icon labelPosition="left">
+          <Icon name="cloud download"></Icon>
+          Télécharger
+        </Button>
+      </a>
+    </Container>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
